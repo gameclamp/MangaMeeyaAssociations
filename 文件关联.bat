a20eb98ad7c21d748aeee9119c681f9234efc01f
9尾雪狐 <gameclamp@gmail.com>
2017-05-28 16:50:17 +0800

    Add files via upload

---
+++ 文件关联.bat
@@ -0,0 +1,43 @@
+REM @ECHO OFF
+set v=%~dp0MangaMeeya.exe
+set v=%v:\=\\%
+> "%Temp%.\DefOpen.reg" ECHO Windows Registry Editor Version 5.00
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\Directory\shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO "Icon"="\"%v%\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\Directory\shell\MangaMeeya\Command]
+>>"%Temp%.\DefOpen.reg" ECHO @="\"%v%\" \"%%1\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.png\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO "Icon"="\"%v%\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.png\Shell\MangaMeeya\Command]
+>>"%Temp%.\DefOpen.reg" ECHO @="\"%v%\" \"%%1\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO "Icon"="\"%v%\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\Shell\MangaMeeya\Command]
+>>"%Temp%.\DefOpen.reg" ECHO @="\"%v%\" \"%%1\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.rar\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO "Icon"="\"%v%\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.rar\Shell\MangaMeeya\Command]
+>>"%Temp%.\DefOpen.reg" ECHO @="\"%v%\" \"%%1\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.zip\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO "Icon"="\"%v%\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.zip\Shell\MangaMeeya\Command]
+>>"%Temp%.\DefOpen.reg" ECHO @="\"%v%\" \"%%1\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.7z\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO "Icon"="\"%v%\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [HKEY_CLASSES_ROOT\SystemFileAssociations\.7z\Shell\MangaMeeya\Command]
+>>"%Temp%.\DefOpen.reg" ECHO @="\"%v%\" \"%%1\""
+>>"%Temp%.\DefOpen.reg" ECHO.
+START /WAIT REGEDIT /S "%Temp%.\DefOpen.reg"
+DEL "%Temp%.\DefOpen.reg"
