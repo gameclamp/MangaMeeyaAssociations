d411d28ea1bf817bae92c3aa00fa8e222d4b6aad
9尾雪狐 <gameclamp@gmail.com>
2018-08-18 18:56:31 +0800

    新增pdf、bmp、webp

--- 文件关联清理.bat
+++ 文件关联清理.bat
@@ -5,15 +5,21 @@ set v=%v:\=\\%
 >>"%Temp%.\DefOpen.reg" ECHO.
 >>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\Directory\shell\MangaMeeya]
 >>"%Temp%.\DefOpen.reg" ECHO.
->>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.png\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.7z\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.bmp\Shell\MangaMeeya]
 >>"%Temp%.\DefOpen.reg" ECHO.
 >>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.jpg\Shell\MangaMeeya]
 >>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.pdf\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO.
+>>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.png\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO.
 >>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.rar\Shell\MangaMeeya]
 >>"%Temp%.\DefOpen.reg" ECHO.
->>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.zip\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.webp\Shell\MangaMeeya]
 >>"%Temp%.\DefOpen.reg" ECHO.
->>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.7z\Shell\MangaMeeya]
+>>"%Temp%.\DefOpen.reg" ECHO [-HKEY_CLASSES_ROOT\SystemFileAssociations\.zip\Shell\MangaMeeya]
 >>"%Temp%.\DefOpen.reg" ECHO.
 START /WAIT REGEDIT /S "%Temp%.\DefOpen.reg"
 DEL "%Temp%.\DefOpen.reg"
